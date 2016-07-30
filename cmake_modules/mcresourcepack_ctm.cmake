c507eadc499253859946ba76e2ebb6bc1ea67888
Adrien Prokopowicz <adrien.prokopowicz@gmail.com>
2016-07-30 02:23:10 +0200

    Updated glass, optimized some textures, and removed the prebuilt pack folder.
    * NEW: The add_ctm_texture() method can now take multiple main variants.
    * NEW: Optimized some textures using the new variants system.
    * NEW: Glass is now relatively darker.
    * NEW: Added 4 random variants to glass.
    * NEW: Removed the prebuilt pack folder.
    * NEW: the add_ctm_texture() can now automatically generate the CTM properties
      files if they are missing.
    * BUG: Fixed the enchanting_table textures.

--- cmake_modules/mcresourcepack_ctm.cmake
+++ cmake_modules/mcresourcepack_ctm.cmake
@@ -52,7 +52,73 @@ set(CTM_CLASSIC_CONTENTS
 
 set(CTM_PARTS BORDER_BOTTOM BORDER_LEFT BORDER_RIGHT BORDER_TOP CORNER_BOTTOMLEFT CORNER_BOTTOMRIGHT CORNER_TOPLEFT CORNER_TOPRIGHT)
 
+function(add_tile_variant _TARGET _TILE_ID _FILE_ID)
+    
+    get_property(TILE_VARIANTS DIRECTORY PROPERTY "CTM_TILE_VARIANTS_${_TARGET}_${_TILE_ID}")
+    set_property(DIRECTORY PROPERTY "CTM_TILE_VARIANTS_${_TARGET}_${_TILE_ID}" ${TILE_VARIANTS} ${_FILE_ID})
+    
+endfunction()
+
 function(add_ctm_texture)
+
+    set(OVA TARGET SOURCE PROPERTIES_FILE ${CTM_PARTS})
+    set(MVA VARIANTS)
+            
+    cmake_parse_arguments(_ "" "${OVA}" "${MVA}" ${ARGN})
+    
+    list_xcf_layers_raw("${CMAKE_CURRENT_LIST_DIR}/${__SOURCE}" LAYERS)
+    
+    list(LENGTH __VARIANTS _VARIANTS_LEN)
+    
+    if("${_VARIANTS_LEN}" EQUAL 0)
+        set(__VARIANTS "^$")
+        set(_VARIANTS_LEN 1)
+    endif()
+    
+    math(EXPR _VARIANTS_LEN "${_VARIANTS_LEN} - 1")
+    
+    set(file_id "0")
+    
+    
+    foreach(i RANGE ${_VARIANTS_LEN})
+        
+        list(GET __VARIANTS ${i} VARIANT)
+        
+        set(remaining_variants ${__VARIANTS})
+        list(REMOVE_ITEM remaining_variants ${VARIANT})
+        
+        add_ctm_texture_base("${LAYERS}" ${file_id}
+            TARGET ${__TARGET}
+            SOURCE ${__SOURCE}
+            PROPERTIES_FILE ${__PROPERTIES_FILE}
+            BORDER_BOTTOM ${__BORDER_BOTTOM}
+            BORDER_LEFT ${__BORDER_LEFT}
+            BORDER_RIGHT ${__BORDER_RIGHT}
+            BORDER_TOP ${__BORDER_TOP}
+            CORNER_BOTTOMLEFT ${__CORNER_BOTTOMLEFT}
+            CORNER_BOTTOMRIGHT ${__CORNER_BOTTOMRIGHT}
+            CORNER_TOPLEFT ${__CORNER_TOPLEFT}
+            CORNER_TOPRIGHT ${__CORNER_TOPRIGHT}
+            ADDLAYERS ${VARIANT}
+            REMOVELAYERS ${remaining_variants}
+        )
+        
+        math(EXPR file_id "${file_id} + 1")
+        
+        set(ALL_BUILD_DESTINATIONS ${ALL_BUILD_DESTINATIONS} ${BUILD_DESTINATIONS})
+        
+    endforeach()
+    
+    add_custom_target("${__TARGET}" ALL
+                    DEPENDS ${ALL_BUILD_DESTINATIONS})
+                    
+    write_ctm_files("${__TARGET}" "${CMAKE_CURRENT_LIST_DIR}/${__PROPERTIES_FILE}")
+                    
+    install(FILES "${CMAKE_CURRENT_LIST_DIR}/${__PROPERTIES_FILE}" DESTINATION "pack/assets/minecraft/mcpatcher/ctm/${__TARGET}/")
+    
+endfunction()
+
+function(add_ctm_texture_base _LAYERS _I)
     
     macro(part_to_variant _OUT_VARIABLE)
         set(${_OUT_VARIABLE} "")
@@ -62,16 +128,17 @@ function(add_ctm_texture)
     endmacro()
     
     set(OVA TARGET SOURCE PROPERTIES_FILE ${CTM_PARTS})
+    set(MVA ADDLAYERS REMOVELAYERS)
             
-    cmake_parse_arguments(_ "" "${OVA}" "" ${ARGN})
+    cmake_parse_arguments(_ "" "${OVA}" "${MVA}" ${ARGN})
     
     set(DESTINATION "${__TARGET}")
-    list_xcf_layers_raw("${CMAKE_CURRENT_LIST_DIR}/${__SOURCE}" LAYERS)
     
     set(BUILD_DESTINATIONS "")
     
     foreach(i RANGE 46)
         list(GET CTM_CLASSIC_CONTENTS ${i} tile_contents)
+        math(EXPR file_id "${i} + ${_I}")
         string(REPLACE "," ";" tile_contents "${tile_contents}")
         
         set(remaining_parts ${CTM_PARTS})
@@ -81,21 +148,52 @@ function(add_ctm_texture)
         part_to_variant(remaining_variants ${remaining_parts})
         
         request_layer_list(OUTPUT_VARIABLE variant_layers
-            LAYER_LIST ${LAYERS}
-            ADD ${tile_variants}
-            REMOVE ${remaining_variants})
+            LAYER_LIST ${_LAYERS}
+            ADD ${tile_variants} ${__ADDLAYERS}
+            REMOVE ${remaining_variants} ${__REMOVELAYERS})
         
         get_filename_component(SOURCE_NAME "${__SOURCE}" NAME)
-        build_destination(BUILD_DESTINATION "${__SOURCE}" "-${i}")
-        
-        add_xcf_file("${CMAKE_CURRENT_LIST_DIR}/${__SOURCE}" "mcpatcher/ctm/${DESTINATION}/${i}.png" "Rendering CTM texture ${SOURCE_NAME} (#${i})" "-${i}" ${variant_layers})
+        build_destination(BUILD_DESTINATION "${__SOURCE}" "-${file_id}")
+        add_xcf_file("${CMAKE_CURRENT_LIST_DIR}/${__SOURCE}" "mcpatcher/ctm/${DESTINATION}/${file_id}.png" "Rendering CTM texture ${SOURCE_NAME} (#${file_id})" "-${file_id}" ${variant_layers})
         list(APPEND BUILD_DESTINATIONS "${CMAKE_CURRENT_LIST_DIR}/${BUILD_DESTINATION}")
         
+        add_tile_variant(${__TARGET} ${i} ${file_id})
+        
     endforeach()
+    set(file_id "${file_id}" PARENT_SCOPE)
+    set(BUILD_DESTINATIONS ${BUILD_DESTINATIONS} PARENT_SCOPE)
     
-    add_custom_target("${__TARGET}" ALL
-                    DEPENDS ${BUILD_DESTINATIONS})
-                    
-    install(FILES "${CMAKE_CURRENT_LIST_DIR}/${__PROPERTIES_FILE}" DESTINATION "pack/assets/minecraft/mcpatcher/ctm/${DESTINATION}/")
+endfunction()
+
+function(write_ctm_files _TARGET _FILE)
+
+    if(NOT EXISTS "${_FILE}")
+        file(WRITE "${_FILE}" 
+                "matchBlocks=<MATCHING BLOCKS HERE>\n"
+                "method=ctm\n"
+                "tiles=0-46\n")
+    endif()
+                         
+    get_filename_component(CTM_DIR "${_FILE}" PATH)
+    get_filename_component(CTM_FILENAME "${_FILE}" NAME_WE)
+    
+    foreach(i RANGE 46)
+        get_property(TILE_VARIANTS DIRECTORY PROPERTY "CTM_TILE_VARIANTS_${__TARGET}_${i}")
+        list(LENGTH TILE_VARIANTS TILE_VARIANTS_LEN)
+        
+        if(${TILE_VARIANTS_LEN} GREATER 1)
+            if(NOT EXISTS "${CTM_DIR}/${CTM_FILENAME}_${i}.properties")
+                string(REPLACE ";" " " TILE_LIST "${TILE_VARIANTS}")
+                file(WRITE "${CTM_DIR}/${CTM_FILENAME}_${i}.properties"
+                    "matchTiles=~/ctm/${__TARGET}/${i}.png\n"
+                    "method=random\n"
+                    "tiles=${TILE_LIST}")
+                
+            endif()
+            
+            install(FILES "${CTM_DIR}/${CTM_FILENAME}_${i}.properties" DESTINATION "pack/assets/minecraft/mcpatcher/ctm/${__TARGET}/")
+        endif()
+        
+    endforeach()
     
 endfunction()
